0160fd7daa2fcd7e0ed8c40b63a3d3d13e702b7c
hinatakudo5 <hinatakudo5@gmail.com>
2026-01-12 18:57:49 +0000

    Fix buns tab click interception in parametrized tabs test

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,51 +1,44 @@
+import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from locators import BASE_URL, MainPageLocators, AccountPageLocators
-from helpers import generate_email, generate_password, generate_name, register_user
 
 
 def test_go_to_constructor_from_account(driver):
     wait = WebDriverWait(driver, 15)
 
-    name = generate_name()
-    email = generate_email(domain="ya.ru")
-    password = generate_password()
-
-    register_user(driver, name, email, password)
-
-    wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT)).click()
+    driver.get(f"{BASE_URL}/account")
     wait.until(EC.element_to_be_clickable(AccountPageLocators.CONSTRUCTOR_LINK)).click()
 
-    wait.until(EC.url_to_be(BASE_URL + "/"))
-    assert driver.current_url == BASE_URL + "/"
+    wait.until(EC.url_to_be(f"{BASE_URL}/"))
+    wait.until(EC.visibility_of_element_located(MainPageLocators.TAB_BUNS))
 
 
 def test_go_to_constructor_by_logo(driver):
     wait = WebDriverWait(driver, 15)
 
-    name = generate_name()
-    email = generate_email(domain="ya.ru")
-    password = generate_password()
-
-    register_user(driver, name, email, password)
-
-    wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT)).click()
+    driver.get(f"{BASE_URL}/account")
     wait.until(EC.element_to_be_clickable(AccountPageLocators.LOGO)).click()
 
-    wait.until(EC.url_to_be(BASE_URL + "/"))
-    assert driver.current_url == BASE_URL + "/"
+    wait.until(EC.url_to_be(f"{BASE_URL}/"))
+    wait.until(EC.visibility_of_element_located(MainPageLocators.TAB_BUNS))
 
 
-def test_constructor_tabs_work(driver):
+@pytest.mark.parametrize(
+    "tab_locator, active_tab_locator, needs_click",
+    [
+        (MainPageLocators.TAB_BUNS, MainPageLocators.ACTIVE_TAB_BUNS, False),
+        (MainPageLocators.TAB_SAUCES, MainPageLocators.ACTIVE_TAB_SAUCES, True),
+        (MainPageLocators.TAB_FILLINGS, MainPageLocators.ACTIVE_TAB_FILLINGS, True),
+    ],
+)
+def test_constructor_tab_opens(driver, tab_locator, active_tab_locator, needs_click):
     wait = WebDriverWait(driver, 15)
+
     driver.get(BASE_URL)
 
-    wait.until(EC.element_to_be_clickable(MainPageLocators.TAB_SAUCES)).click()
-    wait.until(EC.visibility_of_element_located(MainPageLocators.ACTIVE_TAB_SAUCES))
+    if needs_click:
+        wait.until(EC.element_to_be_clickable(tab_locator)).click()
 
-    wait.until(EC.element_to_be_clickable(MainPageLocators.TAB_FILLINGS)).click()
-    wait.until(EC.visibility_of_element_located(MainPageLocators.ACTIVE_TAB_FILLINGS))
-
-    wait.until(EC.element_to_be_clickable(MainPageLocators.TAB_BUNS)).click()
-    wait.until(EC.visibility_of_element_located(MainPageLocators.ACTIVE_TAB_BUNS))
+    wait.until(EC.visibility_of_element_located(active_tab_locator))
